072815dc122b43a56b155286f80e373063d74232
tejas-kale <kaletejas2006@gmail.com>
2016-02-08 12:41:51 +0530

    Second review modification incorporated

--- js/helper.js
+++ js/helper.js
@@ -1,13 +1,13 @@
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span id="role">%data%</span><hr/>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="flex-item"><span class="red-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="red-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="red-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="red-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="red-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="red-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="red-text">location</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
@@ -35,7 +35,7 @@ var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineClasses = '<h3 class="online">Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
@@ -101,7 +101,7 @@ function initializeMap() {
   For the map to be displayed, the googleMap var must be
   appended to #mapDiv in resumeBuilder.js.
   */
-  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+  var map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
   /*
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -15,177 +15,170 @@ Notes
 
 // Bio info
 var bio = {
-	"name": "Tejas Kale",
-	"role": "Web Developer",
-	"contacts": {
-			"mobile": "9876543210",
-		"email": "kaletejas2006@gmail.com",
-		"github": "tejas-kale",
-		"twitter": "@tejaskale",
-		"location": "Pune, India"
+	'name': 'Tejas Kale',
+	'role': 'Web Developer',
+	'contacts': {
+		'mobile': '9876543210',
+		'email': 'kaletejas2006@gmail.com',
+		'github': 'tejas-kale',
+		'twitter': '@tejaskale',
+		'location': 'Pune, India'
 	},
-	"welcomeMessage": "Liberty is not the power of doing what we like, but the right of being able to do what we ought.",
-	"skills": ["Web Development", "Cooking", "Data Analysis"],
-	"biopic": "images/profile_picture.jpg",
+	'welcomeMessage': 'Liberty is not the power of doing what we like, but the right of being able to do what we ought.',
+	'skills': ['Web Development', 'Cooking', 'Data Analysis'],
+	'biopic': 'images/profile_picture.jpg',
 };
 
 // Method to display bio
 bio.display = function() {
 	// Add name and role to header
-	$("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
-	$("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
+	$('#header').prepend(HTMLheaderRole.replace('%data%', bio.role))
+		.prepend(HTMLheaderName.replace('%data%', bio.name));
 
 	// Add contact info
-	$("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile));
-	$("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
-	$("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
-	$("#topContacts").append(HTMLtwitter.replace("%data%", bio.contacts.twitter));
-	$("#topContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
+	$('#topContacts').append(HTMLmobile.replace('%data%', bio.contacts.mobile))
+		.append(HTMLemail.replace('%data%', bio.contacts.email))
+		.append(HTMLgithub.replace('%data%', bio.contacts.github))
+		.append(HTMLtwitter.replace('%data%', bio.contacts.twitter))
+		.append(HTMLlocation.replace('%data%', bio.contacts.location));
+
+	$('#footerContacts').append(HTMLmobile.replace('%data%', bio.contacts.mobile))
+		.append(HTMLemail.replace('%data%', bio.contacts.email))
+		.append(HTMLgithub.replace('%data%', bio.contacts.github))
+		.append(HTMLtwitter.replace('%data%', bio.contacts.twitter))
+		.append(HTMLlocation.replace('%data%', bio.contacts.location));
 
 	// Add picture and welcome message
-	$("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
-	$("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+	$('#header').append(HTMLbioPic.replace('%data%', bio.biopic))
+		.append(HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage));
 
 	// Add skills
 	if (bio.skills.length > 0) {
-		$("#header").append(HTMLskillsStart);
-		for (var x in bio.skills) {
-			if (bio.skills.hasOwnProperty(x)) {
-				$("#skills").append(HTMLskills.replace("%data%", bio.skills[x]));
-			}
+		$('#header').append(HTMLskillsStart);
+		for (var x = 0; x < bio.skills.length; x++) {
+			$('#skills').append(HTMLskills.replace('%data%', bio.skills[x]));
 		}
 	}
 
 	// Add internationalize button
-	$("#header").append(internationalizeButton);
+	$('#header').append(internationalizeButton);
 };
 
 
 // Employment info
 var work = {
-	"jobs": [{
-		"employer": "Cumulus Systems Pvt. Ltd.",
-		"title": "Lead Analyst",
-		"location": "Pune, India",
-		"dates": "March 2014 - present",
-		"description": "Working on forecasting capacity requirements of large storage arrays."
+	'jobs': [{
+		'employer': 'Cumulus Systems Pvt. Ltd.',
+		'title': 'Lead Analyst',
+		'location': 'Pune, India',
+		'dates': 'March 2014 - present',
+		'description': 'Working on forecasting capacity requirements of large storage arrays.'
 	},
 	{
-		"employer": "Inter University Center for Astronomy and Astrophysics",
-		"location": "Pune, India",
-		"title": "Statistician",
-		"dates": "March 2014 - May 2015",
-		"description": "Worked on the development of a statistical package 'AstroStat' that aimed to provide an easy interface for performing common analytical exercises in Astronomy and Astrophysics."
+		'employer': 'Inter University Center for Astronomy and Astrophysics',
+		'title': 'Statistician',
+		'location': 'Pune, India',
+		'dates': 'March 2014 - May 2015',
+		'description': 'Worked on the development of a statistical package AstroStat that aimed to provide an easy interface for performing common analytical exercises in Astronomy and Astrophysics.'
 	}]
 };
 
 // Display work info
 work.display = function() {
-	for (var job in work.jobs) {
-		if (work.jobs.hasOwnProperty(job)) {
-			$("#work_experience").append(HTMLworkStart);
+	for (var job = 0; job < work.jobs.length; job++) {
+		$('#work_experience').append(HTMLworkStart);
 
-			$(".work-entry:last").append(HTMLworkEmployer.replace("%data%", work.jobs[job].employer) + HTMLworkTitle.replace("%data%", work.jobs[job].title));
-			$(".work-entry:last").append(HTMLworkDates.replace("%data%", work.jobs[job].dates));
-			$(".work-entry:last").append(HTMLworkLocation.replace("%data%", work.jobs[job].location));
-			$(".work-entry:last").append(HTMLworkDescription.replace("%data%", work.jobs[job].description))	;
-		}
+		$('.work-entry:last').append(HTMLworkEmployer.replace('%data%', work.jobs[job].employer) + HTMLworkTitle.replace('%data%', work.jobs[job].title))
+			.append(HTMLworkDates.replace('%data%', work.jobs[job].dates))
+			.append(HTMLworkLocation.replace('%data%', work.jobs[job].location))
+			.append(HTMLworkDescription.replace('%data%', work.jobs[job].description));
 	}
 };
 
 
 // Eduction info
 var education = {
-	"schools":[
+	'schools':[
 	{
-		"name": "University of Oxford",
-		"location": "Oxford, UK",
-		"degree": "M.Sc.",
-		"majors": "Applied Statistics",
-		"dates": "October 2010 - October 2011",
-		"url": "http://www.ox.ac.uk/admissions/graduate/courses/msc-applied-statistics"
+		'name': 'University of Oxford',
+		'location': 'Oxford, UK',
+		'degree': 'M.Sc.',
+		'majors': ['Applied Statistics'],
+		'dates': 2011,
+		'url': 'http://www.ox.ac.uk/admissions/graduate/courses/msc-applied-statistics'
 	},
 	{
-		"name": "University of Mumbai",
-		"location": "Mumbai, India",
-		"degree": "B.Sc.",
-		"majors": "Statistics",
-		"dates": "July 2007 - June 2010",
-		"url": "http://www.google.com"
+		'name': 'University of Mumbai',
+		'location': 'Mumbai, India',
+		'degree': 'B.Sc.',
+		'majors': ['Statistics'],
+		'dates': 2010,
+		'url': 'http://www.google.com'
 	}],
-	"onlineCourses": [
+	'onlineCourses': [
 	{
-		"title": "Front-end Web Development",
-		"school": "Udacity",
-		"dates": "October 2015 - present",
-		"url": "http://www.udacity.com"
+		'title': 'Front-end Web Development',
+		'school': 'Udacity',
+		'date': 2016,
+		'url': 'http://www.udacity.com'
 	}]
 };
 
 // Display education info
 education.display = function() {
 	// Append school info
-	for (var school in education.schools) {
-		if (education.schools.hasOwnProperty(school)) {
-			$("#education").append(HTMLschoolStart);
+	for (var school = 0; school < education.schools.length; school++) {
+		$('#education').append(HTMLschoolStart);
 
-			$(".education-entry:last").append(HTMLschoolName.replace("%data%", education.schools[school].name) + HTMLschoolDegree.replace("%data%", education.schools[school].degree));
-			$(".education-entry:last").append(HTMLschoolDates.replace("%data%", education.schools[school].dates));
-			$(".education-entry:last").append(HTMLschoolLocation.replace("%data%", education.schools[school].location));
-			$(".education-entry:last").append(HTMLschoolMajor.replace("%data%", education.schools[school].majors));
-		}
+		$('.education-entry:last').append(HTMLschoolName.replace('%data%', education.schools[school].name) + HTMLschoolDegree.replace('%data%', education.schools[school].degree))
+			.append(HTMLschoolDates.replace('%data%', education.schools[school].dates))
+			.append(HTMLschoolLocation.replace('%data%', education.schools[school].location))
+			.append(HTMLschoolMajor.replace('%data%', education.schools[school].majors));
 	}
 
 	// Append online courses info
-	for (var oc in education.onlineCourses) {
-		if (education.onlineCourses.hasOwnProperty(oc)) {
-			$(".education-entry:last").append(HTMLonlineClasses);
-
-			$(".education-entry:last").append(HTMLonlineTitle.replace("%data%", education.onlineCourses[oc].title) + HTMLonlineSchool.replace("%data%", education.onlineCourses[oc].school));
-			$(".education-entry:last").append(HTMLonlineDates.replace("%data%", education.onlineCourses[oc].dates));
-			$(".education-entry:last").append(HTMLonlineURL.replace("%data%", education.onlineCourses[oc].url));
-		}
+	for (var oc = 0; oc < education.onlineCourses.length; oc++) {
+		$('.education-entry:last').append(HTMLonlineClasses)
+			.append(HTMLonlineTitle.replace('%data%', education.onlineCourses[oc].title) + HTMLonlineSchool.replace('%data%', education.onlineCourses[oc].school))
+			.append(HTMLonlineDates.replace('%data%', education.onlineCourses[oc].date))
+			.append(HTMLonlineURL.replace('%data%', education.onlineCourses[oc].url));
 	}
 };
 
 
 // Projects info
 var projects = {
-	"projects": [{
-		"title": "rd3",
-		"dates": "July 2015 - present",
-		"description": "R package for piecewise construction of D3 visualizations.",
-		"images": []
+	'projects': [{
+		'title': 'rd3',
+		'dates': 'July 2015 - present',
+		'description': 'R package for piecewise construction of D3 visualizations.',
+		'images': []
 	},
 	{
-		"title": "GRG Hunter",
-		"dates": "January 2016 - present",
-		"description": "R package to (computationally) learn features of Giant Radio Galaxies and spot them in new images.",
-		"images": []
+		'title': 'GRG Hunter',
+		'dates': 'January 2016 - present',
+		'description': 'R package to (computationally) learn features of Giant Radio Galaxies and spot them in new images.',
+		'images': []
 	},
 	{
-		"title": "Finance Advisor",
-		"dates": "February 2016 - present",
-		"description": "Python scripts to keep track of income and expenditure for suggesting investment options.",
-		"images": []
+		'title': 'Finance Advisor',
+		'dates': 'February 2016 - present',
+		'description': 'Python scripts to keep track of income and expenditure for suggesting investment options.',
+		'images': []
 	}]
 };
 
 projects.display = function() {
-	for (var project in projects.projects) {
-		if (projects.projects.hasOwnProperty(project)) {
-			$("#projects").append(HTMLprojectStart);
+	for (var project = 0; project < projects.projects.length; project++) {
+		$('#projects').append(HTMLprojectStart);
 
-			$(".project-entry:last").append(HTMLprojectTitle.replace("%data%", projects.projects[project].title));
-			$(".project-entry:last").append(HTMLprojectDates.replace("%data%", projects.projects[project].dates));
-			$(".project-entry:last").append(HTMLprojectDescription.replace("%data%", projects.projects[project].description));
+		$('.project-entry:last').append(HTMLprojectTitle.replace('%data%', projects.projects[project].title))
+			.append(HTMLprojectDates.replace('%data%', projects.projects[project].dates))
+			.append(HTMLprojectDescription.replace('%data%', projects.projects[project].description));
 
-			if (projects.projects[project].images.length > 0) {
-				for (var img in projects.projects[project].images) {
-					if (projects.projects[project].images.hasOwnProperty(img)) {
-						$(".project-entry:last").append(HTMLprojectImage.replace("%data%", projects.projects[project].images[img]));
-					}
-				}
+		if (projects.projects[project].images.length > 0) {
+			for (var img = 0; img < projects.projects[project].images.length; img++) {
+				$('.project-entry:last').append(HTMLprojectImage.replace('%data%', projects.projects[project].images[img]));
 			}
 		}
 	}
@@ -200,4 +193,4 @@ projects.display();
 education.display();
 
 // Append map
-$("#mapDiv").append(googleMap);
+$('#mapDiv').append(googleMap);
